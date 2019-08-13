38f899faeebc9ac0fc955df4154d4ca3f63f759a
saied <saiedmomen@gmail.com>
2019-08-13 20:14:15 +0430

    implement clicks

--- app/src/main/java/dev/saied/flowbindings/sample/MainActivity.kt
+++ app/src/main/java/dev/saied/flowbindings/sample/MainActivity.kt
@@ -1,14 +1,33 @@
 package dev.saied.flowbindings.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.flow.flowOf
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import dev.saied.flowbindings.flowbindings.clicks
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    @ExperimentalCoroutinesApi
+    @FlowPreview
+    @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val flow = flowOf(1, 2, 3)
+        lifecycleScope.launch {
+            button.clicks()
+                .collect(object : FlowCollector<Unit> {
+                    override suspend fun emit(value: Unit) {
+                        Toast.makeText(this@MainActivity, "clicked!", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
     }
+
 }
